2967729ddce46b561859455ed9818cfd6df629ae
Farhaan <fari3001_14@hotmail.com>
2022-02-28 02:13:07 +1000

    crypto app

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,13 +1,24 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': '6688b15502msh8d61a3e27f34e98p143d5ejsnad6acb6d31cb'
 }
 
-const baseURL = 'https://coinranking1.p.rapidapi.com/coins';
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi'
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
 })
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi

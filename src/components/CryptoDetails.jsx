1680fe7e543e7a8fc517d2804ce0e8a459645305
Farhaan <fari3001_14@hotmail.com>
2022-03-18 14:50:00 +1000

    use params

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import HTMLReactParser from 'html-react-parser'
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd'
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+
+
+const { Title, Text } = Typography;
+const { Option } = Select
 
 const CryptoDetails = () => {
+  const { coinId } = useParams();
+  console.log({coinId})
+
   return (
-    <div>CryptoDetails</div>
+    <div>
+      CryptoDetails { coinId }
+    </div>
   )
 }
 
